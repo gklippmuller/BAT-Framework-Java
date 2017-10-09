f8ffc74050f526f9070bdb38836cb454bee5dc67
Miguel DAlessio <miguelangelmuro10@gmail.com>
2017-10-09 17:36:22 -0300

    Log Manager Refactor

---
+++ BAT-Framework-java/com/baufest/bat/core/helpers/logging/LogManager.java
@@ -0,0 +1,45 @@
+package baufest.bat.core.helpers.logging;
+
+public class LogManager {
+
+    private static LogManager instance = null;
+
+    private ScreenLogger screenLogger;
+    private FileLogger fileLogger;
+
+    private LogManager() {
+
+        screenLogger = new ScreenLogger();
+        fileLogger = new FileLogger();
+
+    }
+
+    public static LogManager getInstance() {
+        if (instance == null) {
+            instance = new LogManager();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        screenLogger.log(message);
+        fileLogger.log(message);
+    }
+
+    public void logInfo(String message) {
+        screenLogger.logInfo(message);
+        fileLogger.logInfo(message);
+    }
+
+    public void logWarning(String message) {
+        screenLogger.logWarning(message);
+        fileLogger.logWarning(message);
+    }
+
+    public void logError(String message) {
+        screenLogger.logError(message);
+        fileLogger.logError(message);
+    }
+
+
+}
---
+++ BAT-Framework-java/com/baufest/test/main.java
@@ -0,0 +1,31 @@
+package baufest.test;
+
+public class main {
+
+    static LogManager logManager = new LogManager();
+
+
+    public static void main (String [ ] args) {
+
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+        logManager.setMessage("MENSAJITO");
+
+
+
+    }
+}
